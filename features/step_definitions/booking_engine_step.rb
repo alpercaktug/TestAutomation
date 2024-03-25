6c1f36af03b5541b2d989a957aaedc17ef3a47c8
alpercaktug <alpercktg@gmail.com>
2024-03-25 11:31:17 +0300

    testing github webhook

--- features/step_definitions/booking_engine_step.rb
+++ features/step_definitions/booking_engine_step.rb
@@ -9,7 +9,7 @@ require_relative '../../lib/pages/sections/reservation_details'
 # done
 Given(/^Navigate to the booking page$/) do
   BookingPage.new(@browser).visit_booking_page
-  sleep 3
+  sleep 4
 end
 
 # GET DATA FROM TABLE
